7e2a96b4b988183930ef2c769d9fa2609a1cfd0f
I-Corinthian <simonjoshua312@gmail.com>
2023-12-22 00:26:41 +0530

    pallet_contracts added to node

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -344,6 +344,12 @@ fn testnet_genesis(
 			min_join_bond: 1 * DOLLARS,
 			..Default::default()
 		},
+
+		assets: pallet_assets::GenesisConfig {
+			// This asset is used by the NIS pallet as counterpart currency.
+			assets: vec![(9, get_account_id_from_seed::<sr25519::Public>("Alice"), true, 1)],
+			..Default::default()
+		},
 		transaction_payment: Default::default(),
 	}
 }
