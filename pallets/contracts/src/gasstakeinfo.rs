62cd26e221b9710dae5acbab6cfb567a99d3bf27
Purva-Chaudhari <48947123+Purva-Chaudhari@users.noreply.github.com>
2024-01-02 18:20:40 +0530

    Comment and format

--- pallets/contracts/src/gasstakeinfo.rs
+++ pallets/contracts/src/gasstakeinfo.rs
@@ -7,7 +7,9 @@ use sp_runtime::{
 use sp_std::{prelude::*};
 use frame_system::{pallet_prelude::BlockNumberFor,};
 
-// the struct is defined to store the detiles on whom the deploied contract is delegated to and the block hight 
+// Struct to hold the delegation details of a deployed contract, 
+// i.e the owner of the contract, the account to which it is delegated, 
+// and the block number when the delegation was set. 
 #[derive(Encode, Decode, Clone, PartialEq, Eq, RuntimeDebug, TypeInfo, MaxEncodedLen)]
 #[scale_info(skip_type_params(T))]
 pub struct AccountStakeinfo<T: frame_system::Config> {
@@ -16,7 +18,8 @@ pub struct AccountStakeinfo<T: frame_system::Config> {
 	pub delegate_at: BlockNumberFor<T>,
 }
 
-// the struct is defined to store the informations on the reputaion and the last blockhight the contracts being used 
+// Struct to track the usage metrics of a contract,
+// i.e reputation score and the block height of its most recent usage.
 #[derive(Encode, Decode, Clone, PartialEq, Eq, RuntimeDebug, TypeInfo, MaxEncodedLen)]
 #[scale_info(skip_type_params(T))]
 pub struct ContractScarcityInfo<T: frame_system::Config> {
@@ -25,20 +28,20 @@ pub struct ContractScarcityInfo<T: frame_system::Config> {
 }
 
 impl<T: frame_system::Config> AccountStakeinfo<T> {
-    //fn to update the delegated validator of a contract 
+    // Update the delegate information for a contract.
     pub fn set_new_stakeinfo(
 		owner: T::AccountId,
         delegate_to: T::AccountId,
 	) -> Self{
 		let current_block_number = <frame_system::Pallet<T>>::block_number();
-		Self {          
+		Self {
 			owner,
-            delegate_to,
+            		delegate_to,
 			delegate_at:current_block_number,
 		}
 	}
-    //fn to set the delegated validator of a contract during instantiate
-	pub fn new_stakeinfo(
+    // Create a new stake info instance
+     pub fn new_stakeinfo(
 		owner: T::AccountId,
 	) -> Self {
 		let current_block_number = <frame_system::Pallet<T>>::block_number();
@@ -49,11 +52,10 @@ impl<T: frame_system::Config> AccountStakeinfo<T> {
 			delegate_at:current_block_number,
 		}
 	}
-
 }
 
 impl<T: frame_system::Config> ContractScarcityInfo<T>{
-    //fn to set the reputation value of the contract on instantialte
+        // Initialize the scarcity information for a contract, 
 	pub fn set_scarcity_info()->Self{
 		let current_block_number = <frame_system::Pallet<T>>::block_number();
 		Self{
@@ -61,11 +63,12 @@ impl<T: frame_system::Config> ContractScarcityInfo<T>{
 			recent_blockhight: current_block_number,
 		}
 	}
-    //fn to update the reputation value of the contract when the contract is called 
+       // Updates the contract's reputation based on its usage.
 	pub fn update_scarcity_info(
 		current_reputation: u64,
 		old_block_hight: BlockNumberFor<T>,
 	)-> Self{
+
 		let current_block_hight = <frame_system::Pallet<T>>::block_number();
 		if current_block_hight > old_block_hight{
 		let new_reputation = current_reputation + 1;
@@ -82,9 +85,6 @@ impl<T: frame_system::Config> ContractScarcityInfo<T>{
 			reputation: new_reputation,
 			recent_blockhight: new_recent_blockhight,
 		}
-		}
-		
+		}	
 	}
-
-
 }
