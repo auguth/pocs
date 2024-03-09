d68b6fb0d54d9974522dc718b265cbe77ff7927d
I-Corinthian <simonjoshua312@gmail.com>
2024-03-09 11:07:54 +0530

    all tests pass

--- pallets/staking/src/testing_utils.rs
+++ pallets/staking/src/testing_utils.rs
@@ -158,6 +158,7 @@ pub fn create_validators_with_seed<T: Config>(
 			create_stash_controller::<T>(i + seed, balance_factor, RewardDestination::Staked)?;
 		let validator_prefs =
 			ValidatorPrefs { commission: Perbill::from_percent(50), ..Default::default() };
+		<ValidatorDelegate<T>>::insert(stash.clone(), 4);
 		Staking::<T>::validate(RawOrigin::Signed(controller).into(), validator_prefs)?;
 		let stash_lookup = T::Lookup::unlookup(stash);
 		validators.push(stash_lookup);
@@ -197,6 +198,7 @@ pub fn create_validators_with_nominators_for_era<T: Config>(
 		let balance_factor = if randomize_stake { rng.next_u32() % 255 + 10 } else { 100u32 };
 		let (v_stash, v_controller) =
 			create_stash_controller::<T>(i, balance_factor, RewardDestination::Staked)?;
+		<ValidatorDelegate<T>>::insert(v_stash.clone(), 4);
 		let validator_prefs =
 			ValidatorPrefs { commission: Perbill::from_percent(50), ..Default::default() };
 		Staking::<T>::validate(RawOrigin::Signed(v_controller.clone()).into(), validator_prefs)?;
@@ -221,7 +223,7 @@ pub fn create_validators_with_nominators_for_era<T: Config>(
 		for _ in 0..validators.min(edge_per_nominator as u32) {
 			let selected = rng.next_u32() as usize % available_validators.len();
 			let validator = available_validators.remove(selected);
-			selected_validators.push(validator);
+			selected_validators.push(validator.clone());
 		}
 		Staking::<T>::nominate(
 			RawOrigin::Signed(n_controller.clone()).into(),
