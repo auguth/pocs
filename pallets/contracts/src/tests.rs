aba0447130080e68cf737177d060ef86e715f5ce
I-Corinthian <simonjoshua312@gmail.com>
2025-02-17 22:53:05 +0530

    reward_claim test update

--- pallets/contracts/src/tests.rs
+++ pallets/contracts/src/tests.rs
@@ -637,14 +637,6 @@ where
 	Ok((wasm_binary, code_hash))
 }
 
-fn compile_wasm_module<T>(fixture_name: &str) -> wast::Result<(Vec<u8>, <T::Hashing as Hash>::Output)>
-where
-	T: frame_system::Config,
-{
-	let wasm_binary = include_bytes!("../fixtures/validator_reward_contract1.wasm");
-	let code_hash = T::Hashing::hash(&wasm_binary.to_vec());
-	Ok((wasm_binary.to_vec(), code_hash))
-}
 
 fn initialize_block(number: u64) {
 	System::reset_events();
@@ -6292,152 +6284,8 @@ fn pocs_update_delegate_valid_owner() {
 		assert_eq!(account_stake_info_event.2, CHARLIE);
 		assert_eq!(*account_stake_info_event.3, System::block_number());
 	});
+
 }
-
-fn setup_reward_contract() -> AccountId32 {
-  // Load the WASM binary from the specified file path
-
-	let (wasm, _) = compile_wasm_module::<Test>("validator_reward_contract1").unwrap();
-
-
-    // Instantiate the contract
-		let data: Vec<u8> = vec![
-			0xb3, 0x88, 0x80, 0x3f, 0xd4, 0x35, 0x93, 0xc7, 0x15, 0xfd, 0xd3, 0x1c, 0x61, 0x14, 0x1a, 0xbd,
-			0x04, 0xa9, 0x9f, 0xd6, 0x82, 0x2c, 0x85, 0x58, 0x85, 0x4c, 0xcd, 0xe3, 0x9a, 0x56, 0x84, 0xe7,
-			0xa5, 0x6d, 0xa2, 0x7d, 0xd4, 0x35, 0x93, 0xc7, 0x15, 0xfd, 0xd3, 0x1c, 0x61, 0x14, 0x1a, 0xbd,
-			0x04, 0xa9, 0x9f, 0xd6, 0x82, 0x2c, 0x85, 0x58, 0x85, 0x4c, 0xcd, 0xe3, 0x9a, 0x56, 0x84, 0xe7,
-			0xa5, 0x6d, 0xa2, 0x7d, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-			0x00, 0x00, 0x00, 0x00
-	];
-	
-	let salt: Vec<u8> = vec![
-    0xda, 0x5e, 0xd0, 0xa0, 0x57, 0x3c, 0xf6, 0x7d, 0xa9, 0xd2, 0xf5, 0x77, 0xd3, 0x40, 0xe6, 0x0e,
-    0x0b, 0xad, 0xba, 0xa0, 0xc1, 0xb5, 0x8a, 0x7e, 0xbe, 0xe6, 0x4a, 0x4b, 0x51, 0xa5, 0xb0, 0x57
-	];
-
-	let addr = Contracts::bare_instantiate(
-		BOB,
-		0,
-		GAS_LIMIT,
-		None,
-		Code::Upload(wasm),
-		data_vex8::get_byte_vector().clone(),
-		salt,
-		DebugInfo::Skip,
-		CollectEvents::Skip,
-	)
-	.result
-	.unwrap()
-	.account_id;
-
-    return addr // Return the contract address and owner
-}
-
-fn setup_user_contract() -> AccountId32 {
-    // Load the WASM binary from the specified file path
-	let (wasm, code_hash) = compile_module::<Test>("dummy").unwrap();
-
-    // Instantiate the contract
-	
-	let addr = Contracts::bare_instantiate(
-		ALICE,
-		0,
-		GAS_LIMIT,
-		None,
-		Code::Upload(wasm),
-		vec![],
-		vec![],
-		DebugInfo::Skip,
-		CollectEvents::Skip,
-	)
-	.result
-	.unwrap()
-	.account_id;
-
-    return addr // Return the contract address and owner
-}
-
-
-
-
-#[test]
-fn claim_with_valid_owner_after_update_delegate() {
-	ExtBuilder::default().existential_deposit(50).build().execute_with(|| {
-
-			let _ = Balances::deposit_creating(&ALICE, 1_000_000);
-			let _ = Balances::deposit_creating(&BOB, 1_000_000);
-
-			let bond_value = 10;
-			let bond = Staking::bond(
-				RuntimeOrigin::signed(BOB),
-				bond_value,
-				pallet_staking::RewardDestination::Account(BOB),
-			);
-
-			let reward_contract_address = setup_reward_contract();
-			let contract_address = setup_user_contract();
-
-			let contract_stake_info_1 = ContractScarcityInfo::<Test>::set_scarcity_info();
-			let account_stake_info_1 = AccountStakeinfo::<Test>::set_new_stakeinfo(ALICE,BOB);
-			<ContractStakeinfoMap<Test>>::insert(contract_address.clone(), contract_stake_info_1.clone());
-			<AccountStakeinfoMap<Test>>::insert(contract_address.clone(),account_stake_info_1.clone());
-
-			let contract_stake_info_2 = ContractScarcityInfo::<Test>::set_scarcity_info();
-			let account_stake_info_2 = AccountStakeinfo::<Test>::set_new_stakeinfo(BOB,BOB);
-			<ContractStakeinfoMap<Test>>::insert(reward_contract_address.clone(), contract_stake_info_2.clone());
-			<AccountStakeinfoMap<Test>>::insert(reward_contract_address.clone(),account_stake_info_2.clone());
-
-			let _ = Balances::deposit_creating(&reward_contract_address, 10_000);
-			let initial_balance = <Test as Config>::ContractCurrency::free_balance(&ALICE);
-			let reward_contract_initial_balance = <Test as Config>::ContractCurrency::free_balance(&reward_contract_address);
-
-			let data: Vec<u8> = vec![
-				0xb3, 0x88, 0x80, 0x3f, 0xd4, 0x35, 0x93, 0xc7, 0x15, 0xfd, 0xd3, 0x1c, 0x61, 0x14, 0x1a, 0xbd,
-				0x04, 0xa9, 0x9f, 0xd6, 0x82, 0x2c, 0x85, 0x58, 0x85, 0x4c, 0xcd, 0xe3, 0x9a, 0x56, 0x84, 0xe7,
-				0xa5, 0x6d, 0xa2, 0x7d, 0xd4, 0x35, 0x93, 0xc7, 0x15, 0xfd, 0xd3, 0x1c, 0x61, 0x14, 0x1a, 0xbd,
-				0x04, 0xa9, 0x9f, 0xd6, 0x82, 0x2c, 0x85, 0x58, 0x85, 0x4c, 0xcd, 0xe3, 0x9a, 0x56, 0x84, 0xe7,
-				0xa5, 0x6d, 0xa2, 0x7d, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-				0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-				0x00, 0x00, 0x00, 0x00
-		];
-		
-			assert!(reward_contract_initial_balance > 100, "The contract balance should be greater than zero.");
-
-			// Now attempt to claim reward as ALICE (valid owner)
-      let claim = Contracts::reward_claim(
-                RuntimeOrigin::signed(ALICE),
-                reward_contract_address.clone(),
-                contract_address.clone(),
-								data,
-            );
-						
-			let x = initial_balance + 10;
-			let final_balance = <Test as Config>::ContractCurrency::free_balance(&ALICE);
-			assert_eq!(
-				final_balance, x,
-				"The contract balance should change after a reward claim."
-			);
-
-        });
-}
-
-// #[test]
-// fn claim_with_invalid_owner_after_update_delegate() {
-// 	ExtBuilder::default().existential_deposit(50).build().execute_with(|| {
-//         let _ = Balances::deposit_creating(&ALICE, 1_000_000);
-//         let _ = Balances::deposit_creating(&BOB, 1_000_000);
-
-// 			let bond_value = 10;
-// 			assert_ok!(Staking::bond(
-// 				RuntimeOrigin::signed(BOB),
-// 				bond_value,
-// 				pallet_staking::RewardDestination::Account(BOB),
-// 			));
-
-//             let reward_contract_address = setup_reward_contract();
-//             let contract_address = setup_user_contract();
-
 
 // 			let contract_stake_info = ContractScarcityInfo::<Test>::set_scarcity_info();
 // 			let account_stake_info = AccountStakeinfo::<Test>::set_new_stakeinfo(ALICE,BOB);
@@ -6449,31 +6297,66 @@ fn claim_with_valid_owner_after_update_delegate() {
 // 			<ContractStakeinfoMap<Test>>::insert(reward_contract_address.clone(), contract_stake_info.clone());
 // 			<AccountStakeinfoMap<Test>>::insert(reward_contract_address.clone(),account_stake_info.clone());
 
-// 			let _ = Balances::deposit_creating(&reward_contract_address, 10_000_000);
-// 			let initial_balance = Balances::free_balance(&reward_contract_address);
 
-// 			let data: Vec<u8> = vec![
-// 				0xb3, 0x88, 0x80, 0x3f, 0xd4, 0x35, 0x93, 0xc7, 0x15, 0xfd, 0xd3, 0x1c, 0x61, 0x14, 0x1a, 0xbd,
-// 				0x04, 0xa9, 0x9f, 0xd6, 0x82, 0x2c, 0x85, 0x58, 0x85, 0x4c, 0xcd, 0xe3, 0x9a, 0x56, 0x84, 0xe7,
-// 				0xa5, 0x6d, 0xa2, 0x7d, 0xd4, 0x35, 0x93, 0xc7, 0x15, 0xfd, 0xd3, 0x1c, 0x61, 0x14, 0x1a, 0xbd,
-// 				0x04, 0xa9, 0x9f, 0xd6, 0x82, 0x2c, 0x85, 0x58, 0x85, 0x4c, 0xcd, 0xe3, 0x9a, 0x56, 0x84, 0xe7,
-// 				0xa5, 0x6d, 0xa2, 0x7d, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-// 				0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-// 				0x00, 0x00, 0x00, 0x00
-// 		];
+#[test]
+fn reward_claim_passes_when_delegate_valid() {
+	ExtBuilder::default().existential_deposit(50).build().execute_with(|| {
 
-// 			assert!(initial_balance > 100, "The contract balance should be greater than zero.");
-//             // Attempt to claim reward as BOB (invalid owner)
-//             assert_err!(Contracts::reward_claim(
-//                 RuntimeOrigin::signed(BOB),
-//                 reward_contract_address.clone(),
-//                 contract_address.clone(), // Assuming the reward contract is the same for this example
-// 								data,
-//             ), pallet_contracts::Error::<Test>::InvalidOwner);
+			let reward_contract_address: AccountId32  = AccountId32::new([3u8; 32]);
+			let contract_address: AccountId32 = AccountId32::new([4u8; 32]);
 
-//         });
-// }
+			let contract_stake_info_1 = ContractScarcityInfo::<Test>::set_scarcity_info();
+			let account_stake_info_1 = AccountStakeinfo::<Test>::set_new_stakeinfo(ALICE,BOB);
+			<ContractStakeinfoMap<Test>>::insert(contract_address.clone(), contract_stake_info_1.clone());
+			<AccountStakeinfoMap<Test>>::insert(contract_address.clone(),account_stake_info_1.clone());
+
+			let contract_stake_info_2 = ContractScarcityInfo::<Test>::set_scarcity_info();
+			let account_stake_info_2 = AccountStakeinfo::<Test>::set_new_stakeinfo(BOB,BOB);
+			<ContractStakeinfoMap<Test>>::insert(reward_contract_address.clone(), contract_stake_info_2.clone());
+			<AccountStakeinfoMap<Test>>::insert(reward_contract_address.clone(),account_stake_info_2.clone());
+
+        let input_data = vec![1, 2, 3]; 
+
+        assert_ok!(Pallet::<Test>::reward_claim(
+					RuntimeOrigin::signed(ALICE),
+            reward_contract_address, 
+            contract_address,         
+            input_data,
+            GAS_LIMIT,
+        ));
+    });
+}
+
+#[test]
+fn reward_claim_fails_when_delegate_invalid() {
+	ExtBuilder::default().existential_deposit(50).build().execute_with(|| {
 
 
+			let reward_contract_address: AccountId32  = AccountId32::new([3u8; 32]);
+			let contract_address: AccountId32 = AccountId32::new([4u8; 32]);
 
+			let contract_stake_info_1 = ContractScarcityInfo::<Test>::set_scarcity_info();
+			let account_stake_info_1 = AccountStakeinfo::<Test>::set_new_stakeinfo(ALICE,ALICE);
+			<ContractStakeinfoMap<Test>>::insert(contract_address.clone(), contract_stake_info_1.clone());
+			<AccountStakeinfoMap<Test>>::insert(contract_address.clone(),account_stake_info_1.clone());
+
+			let contract_stake_info_2 = ContractScarcityInfo::<Test>::set_scarcity_info();
+			let account_stake_info_2 = AccountStakeinfo::<Test>::set_new_stakeinfo(BOB,BOB);
+			<ContractStakeinfoMap<Test>>::insert(reward_contract_address.clone(), contract_stake_info_2.clone());
+			<AccountStakeinfoMap<Test>>::insert(reward_contract_address.clone(),account_stake_info_2.clone());
+
+        let input_data = vec![1, 2, 3];
+
+        assert_noop!(
+            Pallet::<Test>::reward_claim(
+							RuntimeOrigin::signed(ALICE),
+							reward_contract_address, 
+							contract_address,         
+							input_data,
+							GAS_LIMIT,
+            ),
+						Error::<Test>::InvalidOwner
+        );
+    });
+}
 
